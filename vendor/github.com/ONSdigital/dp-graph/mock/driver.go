91fa2b2f7fbb4552bd20aad13db3150ff72df618
Eleanor Deal <eleanor.deal@gmail.com>
2019-02-21 16:38:55 +0000

    Remove merge conflicts from dependency

--- vendor/github.com/ONSdigital/dp-graph/mock/driver.go
+++ vendor/github.com/ONSdigital/dp-graph/mock/driver.go
@@ -15,22 +15,10 @@ func (m *Mock) Close(ctx context.Context) error {
 	return nil
 }
 
-<<<<<<< HEAD
 func (m *Mock) Healthcheck() (string, error) {
 	return "mock", nil
 }
 
-func (m *Mock) checkForErrors() error {
-	if m.IsBackendReachable != true {
-		return errors.New("database unavailble - 500")
-	}
-
-	if m.IsQueryValid != true {
-		return errors.New("invalid query - 400")
-	}
-
-	if m.IsContentFound != true {
-=======
 func (m *Mock) checkForErrors() error {
 	if !m.IsBackendReachable {
 		return errors.New("database unavailable - 500")
@@ -41,7 +29,6 @@ func (m *Mock) checkForErrors() error {
 	}
 
 	if !m.IsContentFound {
->>>>>>> cmd-develop
 		return errors.New("not found - 404")
 	}
 
--- vendor/github.com/ONSdigital/dp-graph/mock/instance.go
+++ vendor/github.com/ONSdigital/dp-graph/mock/instance.go
@@ -5,13 +5,5 @@ import (
 )
 
 func (m *Mock) CountInsertedObservations(ctx context.Context, instanceID string) (count int64, err error) {
-<<<<<<< HEAD
-	if err := m.checkForErrors(); err != nil {
-		return 0, err
-	}
-
-	return 100, nil
-=======
 	return 0, m.checkForErrors()
->>>>>>> cmd-develop
 }
